ea5d5f1977e08c75fe53c9353b5ec57edd4179d7
agoes29 <titikawal@DESKTOP-OCBUD65>
2024-02-16 17:11:27 +0800

    add Nav

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="w-full h-auto">
+
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-full sticky top-0 bg-white">
+        <nav class="grid grid-cols-1 items-center h-"></nav>
+    </div>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style lang="css"></style>
