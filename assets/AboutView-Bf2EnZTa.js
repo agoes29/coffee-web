30607d738f4561dcbe84d16692423ad2698af3a8
agoes29 <titikawal@DESKTOP-OCBUD65>
2024-02-23 14:42:02 +0800

    new build

--- assets/AboutView-Bf2EnZTa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as o,o as t}from"./index-CxvWbG-3.js";const c={};function r(n,a){return t(),o("h1",null,"new about")}const f=e(c,[["render",r]]);export{f as default};
